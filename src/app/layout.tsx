facfe64de8b2cf60def15c80d391c2bf0d5790fe
jorge luiz <luizj1718@gmail.com>
2024-09-05 10:04:40 -0300

    remove: Toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { Toaster } from "@/components/ui/toaster"
-
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -28,7 +26,6 @@ export default function RootLayout({
         )}
       >
         {children}
-        <Toaster />
       </body>
     </html>
   );
