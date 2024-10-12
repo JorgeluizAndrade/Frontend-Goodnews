e3cbe25516333279ee170a476bfed0a609314185
jorge luiz <luizj1718@gmail.com>
2024-10-12 20:43:27 -0300

    style

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ import Link from "next/link";
 export const Hero = () => {
   return (
     <div>
-      <section id="hero" className="text-gray-800 body-font">
+      <section id="hero" className="text-black body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <motion.h1
