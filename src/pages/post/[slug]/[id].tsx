417d85302588e4bf76f99978199e37f17fa8d0fb
jorge luiz <luizj1718@gmail.com>
2024-10-13 18:16:57 -0300

    fear metadata

--- src/pages/post/[slug]/[id].tsx
+++ src/pages/post/[slug]/[id].tsx
@@ -2,7 +2,13 @@ import { GetStaticProps, GetStaticPaths } from "next";
 import { Post } from "@/components/Post";
 import { useRouter } from "next/router";
 import "@/app/globals.css";
+import type { Metadata, ResolvingMetadata } from "next";
+import Head from "next/head";
 
+type Props = {
+  params: { slug: string; id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
 export default function PostPage({ post }: { post: any }) {
   const router = useRouter();
@@ -16,7 +22,16 @@ export default function PostPage({ post }: { post: any }) {
   };
 
   return (
+    <>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="description" content={post.text} />
+        <meta property="og:title" content={post.title} />
+        <meta property="og:image" content="https://pin.it/5rkMf9K3h" />
+      </Head>
+
       <Post slug={post.slug} id={post.id} />
+    </>
   );
 }
 
@@ -34,7 +49,9 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { slug, id } = params as { slug: string; id: string };
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_GOODNEWS_API}/api/posts/${slug}/${id}`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_GOODNEWS_API}/api/posts/${slug}/${id}`
+  );
   const post = await res.json();
 
   return {
